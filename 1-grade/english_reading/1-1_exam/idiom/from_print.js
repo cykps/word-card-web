ec013a23ead0ed2f45d73cf605185399c12e20cc
cykps <cykps.ps@gmail.com>
2023-06-06 08:12:06 +0900

    add idiom

---
+++ 1-grade/english_reading/1-1_exam/idiom/from_print.js
@@ -0,0 +1,13 @@
+const quizData = [
+    {"ex" : "[  ] foreign tourist (外国人観光客の間で)", "word" : "among"},
+    {"ex" : "AはBで有名", "word" : "A is famous for B"},
+    {"ex" : "Aの心を引きつける", "word" : "appeal to A"},
+    {"ex" : "Aだけでなく、Bも", "word" : "not only A but also B"},
+    {"ex" : "AとBはXの二つの例です。", "word" : "A and B are two examples of X."},
+    {"ex" : "Aは、Bを表します。", "word" : "A stand for B."},
+    {"ex" : "A is [  ] popular (Aは特に人気だ)", "word" : "especially"},
+    {"ex" : "Aは今、Bのメッカです。", "word" : "A is now a mecca for B."},
+    {"ex" : "中には", "word" : "some of them"},
+    {"ex" : "AやBなど", "word" : "such as A and B"},
+    {"ex" : "忍者修行のレッスンを受ける", "word" : "take ninja training lesson"}
+];
