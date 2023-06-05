f9042d02e8c02e5f3a5a6232b0a9a121c208ab91
cykps <cykps.ps@gmail.com>
2023-06-05 21:40:31 +0900

    add stady note questions

---
+++ 1-grade/english_reading/1-1_exam/stady_note_questions/from_print.js
@@ -0,0 +1,33 @@
+const quizData = [
+    {"ex" : "I [ (be) ] a tennis player. (現在形)", "word" : "am"},
+    {"ex" : "We [ (be) ] in Yokohama yesterday.", "word" : "were"},
+    {"ex" : "My brother often [ (みる) ] movies.", "word" : "watches"},
+    {"ex" : "He [ (visit) ] the museum last week.", "word" : "visited"},
+
+    {"ex" : "He [  ] his homework in the library now.", "word" : "is doing"},
+    {"ex" : "They [ (話す) ] on the phone at that time.", "word" : "were talking"},
+    {"ex" : "What [  ] she doing when you visited her?", "word" : "was"},
+    {"ex" : "Kate is [ (学ぶ) ] Japanese from Taku.", "word" : "learning"},
+
+    {"ex" : "You [  ] see Mt. Fuji from here on sunny days.", "word" : "can"},
+    {"ex" : "He should [ (練習する) ] soccer harder.", "word" : "practice"},
+    {"ex" : "She [  ] read a book at home tomorrow.", "word" : "will"},
+    {"ex" : "That man [  ] be a famous person. (~に違いない)", "word" : "must"},
+
+    {"ex" : "Hana [ (行く) ] to school with Emma every morning.", "word" : "goes"},
+    {"ex" : "John and Bob [  ] be tried. (~に違いない)", "word" : "must"},
+    {"ex" : "My father [  ] looking at the website. (今)", "word" : "is"},
+    {"ex" : "When my brother got home, I [  ] listening to music in my room.", "word" : "was"},
+
+    {"ex" : "あなたのお父さんは毎朝、朝食を作りますか。(cook)", "word" : "Does your father cook breakfast every morning?"},
+    {"ex" : "明日は暑くなることでしょう。", "word" : "It will be hot tomorrow."},
+    {"ex" : "私は今、(私の)くつを洗っているところです。", "word" : "I am washing my shoes now."},
+    {"ex" : "エリが電話してきたとき、私はテレビを見ていました。(,を使って)", "word" : "When Eri called me, I was watching TV."},
+    {"ex" : "There are shrines [  ] [  ] Japan. (日本の至る所に神社があります。)", "word" : "all over"},
+    {"ex" : "That singer is [  ] [  ] young man. (その歌手は若い男性に人気があります。)", "word" : "popular with"},
+    {"ex" : "This town is [  ] [  ] an old station. (この町は古い駅で有名です。)", "word" : "famous for"},
+    {"ex" : "[ (読む) ] books is the most interesting things for him.", "word" : "Reading"},
+    {"ex" : "I love [ (走る) ] in the park early in the morning.", "word" : "running"},
+    {"ex" : "She left the room without [ (言う) ] a word", "word" : "saying"},
+    {"ex" : "My father's job is [ (教える) ] math at a high school.", "word" : "teaching"}
+];
