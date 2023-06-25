792ce17641f3b6875ebd0b232260a0ae3d318c1b
cykps <cykps.ps@gmail.com>
2023-06-25 19:37:24 +0900

    add chemistry quiz

---
+++ 1-grade/chemistry/1_2_d-3/data.js
@@ -0,0 +1,5 @@
+const quizData = [
+    { "ex": "塩素の検出方法と、化学反応式", "word": "Cl<sup>-</sup>にして、硝酸銀AgNO<sub>3</sub>水溶液を加える。(逆に、銀の検出にも使える) Ag<sup>+</sup>+Cl<sup>-</sup>→AgCl (白色沈殿)"},
+    { "ex": "炭素の検出方法と、化学反応式", "word": "CO<sub>2</sub>にして、石灰水に通す。CO<sub>2</sub>+Ca(OH)<sub>2</sub>→CaCO<sub>3</sub>+H<sub>2</sub>O"},
+    { "ex": "水素の検出方法2つ", "word": "H<sub>2</sub>Oにして、①塩化コバルト紙にふれさせる(赤変する)。②硫酸銅(II)無水物にふれさせる(青色になる)。"}
+]
